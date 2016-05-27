d74d54052355a290657bbe6cf76e01b95ddacc81
Gabriel Csapo <gabecsapo@gmail.com>
2016-05-27 12:24:40 -1000

    adds logging to all git server related actions

--- index.js
+++ index.js
@@ -40,16 +40,59 @@ repos.forEach(function(repo) {
 });
 
 server = new GitServer(repos, true, path.resolve(__dirname, 'repos'), 7000);
-// TODO: log this action
-server.on('commit', function(update, repo) {
+
+server.on('pre-receive', function(update, repo) {
+    log.info('git:pre-receive', repo.name);
     update.accept();
 });
-// TODO: log this action
+
 server.on('update', function(update, repo) {
+    log.info('git:update', repo.name);
+    update.accept();
+});
+
+server.on('pre-rebase', function(update, repo) {
+    log.info('git:pre-rebase', repo.name);
+    update.accept();
+});
+
+server.on('pre-auto-gc', function(update, repo) {
+    log.info('git:pre-auto-gc', repo.name);
+    update.accept();
+});
+
+server.on('commit-msg', function(update, repo) {
+    log.info('git:commit-msg', repo.name);
+    update.accept();
+});
+
+server.on('prepare-commit-msg', function(update, repo) {
+    log.info('git:prepare-commit-msg', repo.name);
+    update.accept();
+});
+
+server.on('pre-commit', function(update, repo) {
+    log.info('git:pre-commit', repo.name);
+    update.accept();
+});
+
+server.on('applypatch-msg', function(update, repo) {
+    log.info('git:applypatch-msg', repo.name);
+    update.accept();
+});
+
+server.on('commit', function(update, repo) {
+    log.info('git:commit', repo.name);
+    update.accept();
+});
+
+server.on('update', function(update, repo) {
+    log.info('git:update', repo.name);
     update.accept();
 });
 server.on('post-update', function(update, repo) {
-    console.log('redeploy the app, build and start');
+    log.info('git:post-update', repo.name);
+    log.info('redeploy the app, build and start');
     // TODO: implement the deploy and starting the app
     // TODO: use kue to create a priority queue to not block main process
     // TODO: deploy should clone the repo to app and do a npm install
@@ -57,6 +100,7 @@ server.on('post-update', function(update, repo) {
     update.accept();
 });
 server.on('fetch', function(update, repo) {
+    log.info('git:fetch', repo.name);
     update.accept();
 });
 
