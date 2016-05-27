fb5b7dc5a15d1dceb6f7153b10949395c17cbbab
Gabriel Csapo <gabecsapo@gmail.com>
2016-05-26 19:33:39 -1000

    pretty prints json files when saving them

--- index.js
+++ index.js
@@ -8,23 +8,26 @@ var crypto = require('crypto');
 // TODO: implement bunyan as a logger
 
 // TODO: abstract into lib/startup.js
+// TODO: use lowdb, because eventually we want to be able to write to these files
+// TODO: or abstract this out to be able to save the json files whenever you change the underlying object?
+
 if (!fs.existsSync(path.resolve(__dirname, 'config', 'user.json'))) {
     var user = {
         username: 'root',
         password: crypto.randomBytes(20).toString('hex')
     }
-    fs.writeFileSync(path.resolve(__dirname, 'config', 'user.json'), JSON.stringify(user));
+    fs.writeFileSync(path.resolve(__dirname, 'config', 'user.json'), JSON.stringify(user, null, 4));
 }
 if (!fs.existsSync(path.resolve(__dirname, 'config', 'repos.json'))) {
     var repos = [{
         name: 'test',
         anonRead: false,
         users: [{
-            user: u ser,
+            user: user,
             permissions: ['R', 'W']
         }]
     }];
-    fs.writeFileSync(path.resolve(__dirname, 'config', 'repos.json'), JSON.stringify(repos));
+    fs.writeFileSync(path.resolve(__dirname, 'config', 'repos.json'), JSON.stringify(repos, null, 4));
 }
 // TODO: add checks to make sure the data is not malformed
 var user = require('./config/user.json');
