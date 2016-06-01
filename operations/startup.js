60341aaba65b123ee6d6a959773963091d9171dc
Gabriel Csapo <gabecsapo@gmail.com>
2016-05-31 14:58:08 -1000

    adds test updates screenshots, refines logic, adds data table (#2)
    * adds test updates screenshots, refines logic, adds data table
    adds test repo into tests folder
    
    * adds test repo to eslint ignore
    replaces cross-spawn-async with cross-spawn
    adds try catch for unlink
    
    * fixes test errors
    
    * adds lowdb to package.json
    
    * adds config folder if it does not exist
    
    * updates travis build config to have the needed build tools for nodegit
    
    * updates travis build script
    
    * Trigger
    
    * fixes yml linting
    
    * updates build job
    
    * fixes build scripts and removes cache
    
    * fix ;

--- operations/startup.js
+++ operations/startup.js
@@ -1,4 +1,8 @@
+var fs = require('fs');
+var path = require('path');
+
 module.exports = (function() {
+    fs.mkdir(path.resolve(__dirname, '..', 'config'),function(){});
     require('./startup-admin.js')();
     require('./startup-gitserver.js')();
 }());
