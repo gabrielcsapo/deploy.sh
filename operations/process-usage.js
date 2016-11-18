d7e7c8a1fc5ab4fdd00a016aedff669ad0cac005
Gabriel Csapo <gacsapo@paypalcorp.com>
2016-11-18 15:45:07 -0800

    show pm2 information on application specific page - backend   - passes created_at, status, instances from process-usage to parent process   - startup admin emits events for {instance}-uptime which emits created_at, status, instances - frontend   - shows the status and instances on application-list page   - shows the status, instances and uptime on application-view page

--- operations/process-usage.js
+++ operations/process-usage.js
@@ -11,6 +11,9 @@ setInterval(function() {
           }
           list.forEach(function(item) {
               process.send({
+                  status: item.pm2_env.status,
+                  created_at: item.pm2_env.created_at,
+                  instances: item.pm2_env.instances,
                   name: item.name,
                   monit: item.monit
               });
