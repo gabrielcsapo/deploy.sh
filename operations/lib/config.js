1374803852ffce8f2a307be6cd6ad90938515ae2
Gabriel Csapo <gabecsapo@gmail.com>
2016-11-20 10:47:11 -0800

    add cli command to regenerate user - backend     - adds cli command to regerate user in config.json     - updates docs to show cli command usage     - adds generate function on config         - adds tests     - fixes config

--- operations/lib/config.js
+++ operations/lib/config.js
@@ -2,22 +2,10 @@ var path = require('path');
 var fs = require('fs');
 var crypto = require('crypto');
 
-var config;
-if(!fs.existsSync(path.resolve(__dirname, '..', '..', 'config', 'config.json'))) {
-    config = {
-        "user": {
-            username: 'root',
-            password: crypto.randomBytes(20).toString('hex')
-        },
-        "repos": []
-    };
-    fs.writeFileSync(path.resolve(__dirname, '..', '..', 'config', 'config.json'), JSON.stringify(config, null, 4));
-} else {
-    config = require('../../config/config.json');
-}
-
-module.exports = {
+var Config = {
     get: function(name) {
+        delete require.cache[require.resolve('../../config/config.json')];
+        var config = require('../../config/config.json');
         if(name) {
             return config[name];
         } else {
@@ -25,17 +13,42 @@ module.exports = {
         }
     },
     update: function(data, callback, name) {
+        delete require.cache[require.resolve('../../config/config.json')];
+        var config = require('../../config/config.json');
         if(name) {
             config[name] = data;
         } else {
             config = data;
         }
         fs.writeFile(path.resolve(__dirname, '..', '..', 'config', 'config.json'), JSON.stringify(config, null, 4), function(err) {
-            if (err) {
-                callback(err);
-            } else {
-                callback(undefined)
-            }
+            callback(err || undefined)
         });
+    },
+    generate: function() {
+        var config;
+        if(!fs.existsSync(path.resolve(__dirname, '..', '..', 'config', 'config.json'))) {
+            config = {
+                "user": {
+                    username: 'root',
+                    password: crypto.randomBytes(20).toString('hex')
+                },
+                "repos": []
+            };
+        } else {
+            config = require('../../config/config.json');
+            // just update the user object
+            config.user = {
+                username: 'root',
+                password: crypto.randomBytes(20).toString('hex')
+            }
+        }
+        fs.writeFileSync(path.resolve(__dirname, '..', '..', 'config', 'config.json'), JSON.stringify(config, null, 4));
+        return config;
     }
+};
+
+if(!fs.existsSync(path.resolve(__dirname, '..', '..', 'config', 'config.json'))) {
+    Config.generate();
 }
+
+module.exports = Config;
