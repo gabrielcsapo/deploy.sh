7e4361efe627b29ebd6c2fe9c4b6e2f5b11d0a69
Gabriel Csapo <gabecsapo@gmail.com>
2017-07-31 01:59:37 -0700

    wip

--- bin/distribute-deploy.js
+++ bin/distribute-deploy.js
@@ -5,27 +5,31 @@ const ora = require('ora');
 const path = require('path');
 
 tar.c({
-    gzip: true,
-    portable: true,
-    file: 'distribute.tgz'
-  }, fs.readdirSync(process.cwd())
-).then(() => {
+  gzip: true,
+  portable: true,
+  file: 'distribute.tgz'
+}, fs.readdirSync(process.cwd())).then(() => {
   const spinner = ora(`Uploading application bundle`).start();
 
   const distribute = fs.createReadStream(path.resolve(process.cwd(), 'distribute.tgz'));
 
-  request.post({ url:'http://0.0.0.0:5000/upload', formData: {
-    name: path.basename(process.cwd()),
-    distribute
-  }}, function optionalCallback(err, httpResponse, body) {
+  request.post({
+    url: 'http://0.0.0.0:5000/upload',
+    formData: {
+      name: path.basename(process.cwd()),
+      distribute
+    }
+  }, function optionalCallback(err, httpResponse, body) {
     if (err) {
       return spinner.fail('Deployment failed 🙈');
     }
     const response = JSON.parse(body);
-    if(response.error) {
-       spinner.fail(`Upload failed ${response.error}`);
+    if (response.error) {
+      spinner.fail(`Upload failed ${response.error}`);
     } else {
-        spinner.succeed(`Upload succeed ${response.url}`);
+      spinner.succeed(`Upload succeed ${response.url}`);
     }
   });
-}).catch((err) => { console.log(err); }); // eslint-disable-line
+}).catch((err) => {
+  console.log(err);
+}); // eslint-disable-line
