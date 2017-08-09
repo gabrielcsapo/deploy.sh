c6fc8cd91da6f0ec0c23143aea3598949587c40c
Gabriel Csapo <gabecsapo@gmail.com>
2017-08-08 22:40:08 -0700

    fixes linting

--- bin/deploy-server.js
+++ bin/deploy-server.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-const Async = require('async');
-const inquirer = require('inquirer');
-
 const program = require('commander');
 program
     .option('-u, --url [url]', 'The endpoint of the deploy.sh server', 'http://localhost:5000')
